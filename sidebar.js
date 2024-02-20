a55a87fa74c4d9866649529ebf3c34056570d22b
omaaj <juwiz999@gmail.com>
2024-02-20 16:03:53 -0500

    worked on main

--- sidebar.js
+++ sidebar.js
@@ -1,6 +1,7 @@
 console.log("Working this file out for a purpose");
 console.log("Working this file out");
-console.log("Holla");
+console.log("Holla main");
 console.log("working on sam again");
-console.log("working on whats");
+console.log("working on whats main");
 console.log("working on whats again");
+console.log("solid main");
