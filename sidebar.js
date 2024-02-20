2d6010cf3c6eb3da07702f3ff26bd7414356c532
omaaj <juwiz999@gmail.com>
2024-02-20 16:01:30 -0500

    finsied with dev

--- sidebar.js
+++ sidebar.js
@@ -1,6 +1,5 @@
 console.log("Working this file out for a purpose");
 console.log("Working this file out");
-console.log("Holla");
-console.log("working on sam again");
+console.log("Holla dev");
 console.log("working on whats");
-console.log("working on whats again");
+console.log("working on whats again dev");
