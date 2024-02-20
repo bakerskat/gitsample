845023c84b47ea8c60a071b2ab80996db2c7b3e8
omaaj <juwiz999@gmail.com>
2024-02-20 16:14:42 -0500

    megeed completely

--- sidebar.js
+++ sidebar.js
@@ -1,7 +1,6 @@
 console.log("Working this file out for a purpose");
 console.log("Working this file out");
 console.log("Holla main");
-
 console.log("working on whats main");
 console.log("working on whats again dev");
 console.log("solid main");
