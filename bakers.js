390491e85a4d34c4fdac887f4749fd6a4b29b5d4
bakerskat <bakerskathleen123@gmail.com>
2024-02-26 04:53:34 -0500

    all done

--- bakers.js
+++ bakers.js
@@ -2,5 +2,5 @@ console.log("Bakers is really amazing in feature2");
 console.log("Bakers is really amazing in feature 1");
 console.log("Bakers is really amazing with sam");
 console.log(
-  "Bakers is really amazing with sam now im working on baker github account"
+  "Bakers is really amazing with sam now im working on baker github accounts"
 );
